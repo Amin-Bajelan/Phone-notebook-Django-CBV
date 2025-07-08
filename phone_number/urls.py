6c95f2a03f91f1323b0375b13fc0a136a41a7e7a
Amin-Bajelan <m.amin.bajelan@gmail.com>
2025-07-09 00:20:04 +0330

    create template contact detail and profile

--- phone_number/urls.py
+++ phone_number/urls.py
@@ -4,6 +4,9 @@ from . import views
 
 urlpatterns = [
     path('list-contact/',views.ListContact.as_view(),name='list-contact'),
-    path('contact/detail/<int:id>',views.ContactDetail.as_view(),name='detail-contact'),
-    path('add-contact/',views.CreateContact.as_view(),name='add-contact'),
+    path('contact/detail/<int:pk>',views.ContactDetail.as_view(),name='detail-contact'),
+    path('add_contact/',views.CreateContact.as_view(),name='add-contact'),
+    path('show_profile/',views.ShowProfile.as_view(),name='profile'),
+    path('delete_contact/<int:pk>',views.DeleteContact.as_view(),name='delete-contact'),
+
 ]
--- phone_number/views.py
+++ phone_number/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.shortcuts import render, redirect
+from django.views.generic import (ListView, DetailView, CreateView,
+                                  UpdateView, DeleteView, TemplateView)
 from django.contrib.auth.mixins import LoginRequiredMixin
 from phone_number.models import Contact
+from accounts.models import Profile
 
 
 # Create your views here.
@@ -14,12 +16,11 @@ class ListContact(ListView):
     model = Contact
     context_object_name = 'contacts'
     template_name = 'phone_number/list_contact.html'
-    paginate_by = 10
+    paginate_by = 5
 
     def get_queryset(self):
         print(self.request.user)
         return Contact.objects.filter(owner=self.request.user)
-
 
 
 class ContactDetail(DetailView):
@@ -30,15 +31,43 @@ class ContactDetail(DetailView):
     context_object_name = 'contact'
     template_name = 'phone_number/detail_contact.html'
 
+    def get_context_data(self, **kwargs):
+        Contact.objects.get(id=self.kwargs['pk'])
+
+
 class CreateContact(CreateView):
     """
     add new contact on login account
     """
     model = Contact
-    fields = ['contact_image', 'contact_name', 'name_contact', 'phone_number', 'email']
+    fields = ['contact_image', 'name_contact', 'phone_number', 'email']
     template_name = 'phone_number/create_contact.html'
     success_url = '/phone_number/list-contact/'
+
     def form_valid(self, form):
         form.instance.owner = self.request.user
-        if form.instance.phone_number is None:
-            pass
+        form.save()
+        return redirect('/phone_number/list-contact/')
+
+
+class ShowProfile(TemplateView):
+    """
+    show profile on login account
+    """
+    model = Profile
+    template_name = "phone_number/profile.html"
+    context_object_name = 'profile'
+
+    def get_object(self):
+        return Profile.objects.get(username=self.request.user)
+
+
+class DeleteContact(DeleteView):
+    model = Contact
+    template_name = "phone_number/delete_contact.html"
+    success_url = '/phone_number/list-contact/'
+
+
+class DetailContact(DetailView):
+    model = Contact
+    template_name = 'phone_number/detail_contact.html'
